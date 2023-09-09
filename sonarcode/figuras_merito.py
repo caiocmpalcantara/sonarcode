a76ae4a3e748b0147ecbd98264d95dbbac2fe93d
caiocmpalcantara <142508139+caiocmpalcantara@users.noreply.github.com>
2023-09-09 15:20:25 -0300

    Update figuras_merito.py
    Insertion of plot_confusion_matrix

--- sonarcode/figuras_merito.py
+++ sonarcode/figuras_merito.py
@@ -30,7 +30,35 @@ def plota_confusao(model,x, y):
   ax.set_title("Confusion Matrix", fontsize=18)
   ax.set_ylabel("True label")
   ax.set_xlabel("Predicted Label")
-  plt.tight_layout()
+  plt.tight_layout()  
+
+def plot_confusion_matrix(cm, cms,  classes,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45, fontsize = 20)
+    plt.yticks(tick_marks, classes, fontsize = 20)
+
+    thresh = cm.max() / 2.
+
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            plt.text(j, i, '{0:.2f}%'.format(cm[i, j]) + '\n$\pm$' + '{0:.2f}%'.format(cms[i, j]),
+                     horizontalalignment="center",
+                     verticalalignment="center", fontsize=25,
+                     color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+
+    # plt.ylabel('Classe Verdadeira', fontsize = 25)
+    # plt.xlabel('Classe Predita', fontsize = 25)
+    plt.ylabel('True Class', fontsize = 25)
+    plt.xlabel('Predicted Class', fontsize = 25)
   
 
 # Curva de validação
