092e82adf6febefde444aee52a76adcd507b8fb1
caiocmpalcantara <142508139+caiocmpalcantara@users.noreply.github.com>
2023-11-08 05:55:15 -0300

    Update modelo.py
    Inserção do modelolstm2 para bidirecional

--- sonarcode/modelo.py
+++ sonarcode/modelo.py
@@ -26,6 +26,21 @@ def modelolstm(n_timesteps, n_features, n_outputs, model_config):
   model.compile(optimizer= model_config.optimizer, loss= model_config.loss, metrics=[model_config.metrics])
   return model
 
+def modelolstm2(n_timesteps, n_features, n_outputs, model_config):
+  model = Sequential()
+  model.add(Bidirectional(LSTM(model_config.neulstm_1, input_shape=(n_timesteps,n_features))))
+  if model_config.use_drop:
+    model.add(Dropout(model_config.drop))
+  if model_config.use_flatten:
+    model.add(Flatten())
+  if model_config.neumlp_1 != 0:
+    model.add(Dense(model_config.neumlp_1, activation=model_config.funcactiv))
+    if model_config.use_drop:
+      model.add(Dropout(model_config.drop))
+  model.add(Dense(n_outputs, activation=model_config.funcout))
+  model.compile(optimizer= model_config.optimizer, loss= model_config.loss, metrics=[model_config.metrics])
+  return model
+
 def modelomlp(data, num_classes, model_config):
   n_steps = data.shape[1]
   model = Sequential()
